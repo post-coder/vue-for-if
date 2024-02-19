defad861d8ec329b51759c1745214921f7c4dce9
post-coder <gabrielspanu.dev@gmail.com>
2024-02-19 11:59:08 +0100

    Carosello: inizio

--- js/script.js
+++ js/script.js
@@ -6,34 +6,10 @@ createApp({
     data() {
         return {
 
-            contatore: 0,
-            nome: '',
-
-            nomi: [
-                'Christian',
-                'Francesco',
-                'Mattia'
-            ]
-            
         }
     },
     methods: {
-        // qui ci possiamo scrivere tutte le funzioni che vogliamo
-        aumentaNumero() {
-            // dentro i metodi di Vue, ritorna al 100% la sintassi originale di JS
-
-            // per accedere ad una proprietà che abbiamo salvato nei data
-            // dobbiamo scrivere il "this." davanti al nome della variabile
-            
-            this.contatore++;
-            // cambiando il valore di contatore, si aggiorna ovunque abbiamo richiamato questa variabile
-
-        },
-
-        saluta() {
-            alert("Ciao " + this.nome)
-        }
-
+        
     },
 }).mount('#app');
 
