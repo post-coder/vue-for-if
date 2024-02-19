112dd43d6c08e7f8a431dcfd9c119c16980aa942
post-coder <gabrielspanu.dev@gmail.com>
2024-02-19 11:44:16 +0100

    function

--- js/script.js
+++ js/script.js
@@ -6,27 +6,28 @@ createApp({
     data() {
         return {
 
-            persona: {
-                nome: 'Mario',
-                cognome: 'Rossi',
-                eta: 19,
-                ruolo: 'Data Analyst',
-            },
-
-
-            coloriPreferiti: [
-                'rosso',
-                'verde',
-                'blu',
-                'arancione',
-                'viola',
-                'aquamarine'
-            ],
-
-            mostraColori: true,
+            contatore: 0,
             
         }
-    }
+    },
+    methods: {
+        // qui ci possiamo scrivere tutte le funzioni che vogliamo
+        aumentaNumero() {
+            // dentro i metodi di Vue, ritorna al 100% la sintassi originale di JS
+
+            // per accedere ad una proprietà che abbiamo salvato nei data
+            // dobbiamo scrivere il "this." davanti al nome della variabile
+            
+            
+            this.contatore++;
+
+            if(this.contatore >= 20) {
+                this.contatore = 0;
+            }
+
+        },
+
+    },
 }).mount('#app');
 
 
